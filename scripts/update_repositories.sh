da9dc961d3fdc3877625d0caaa08a3fde2786be4
Mark Whitfeld <mark.whitfeld@gmail.com>
2023-08-25 23:09:21 +0200

    fix: improve update repositories script

--- scripts/update_repositories.sh
+++ scripts/update_repositories.sh
@@ -5,12 +5,12 @@ repository_name="$2"
 branch_name="$3"
 commit_hash="$4"
 
-file_path="../metadata/repositories.json"
+file_path="$(dirname $0)/../metadata/repositories.json"
 
 if [ ! -f "$file_path" ]; then
   echo "{}" > "$file_path"
 fi
 data=$(cat "$file_path")
 new_data="{\"$repository_ref\": { \"name\": \"$repository_name\", \"branches\": { \"$branch_name\": { \"commit\": \"$commit_hash\" } } } }"
-json_data=$(jq ". * $new_data <<< "$data")
+json_data=$(jq ". * $new_data" <<< "$data")
 echo "$json_data" > "$file_path"
