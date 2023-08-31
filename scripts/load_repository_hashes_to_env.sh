08d5c56c7669497877cf43f2d7c598ab0f4c3ad8
Mark Whitfeld <mark.whitfeld@gmail.com>
2023-09-01 00:53:22 +0200

    feat: add deployment workflows

--- scripts/load_repository_hashes_to_env.sh
+++ scripts/load_repository_hashes_to_env.sh
@@ -2,18 +2,20 @@
 
 file_path="$(dirname "${BASH_SOURCE[0]}")/../metadata/repositories.json"
 
+branch_name=$1
+
 jq_query='
   def getCommit(branch_name): .[branch_name].commit | if . == "" then empty else . end;
   def getAssignment(name; value): 
     if name and name != "" and value and value != "" then "\(name)=\(value)" else empty end;
 
   .[]
-  | . + { hash: .branches | (getCommit("markwhitfeld/test-branch-sync") // getCommit("main")) }
+  | . + { hash: .branches | (getCommit($branch) // getCommit("main")) }
   | getAssignment(.env; .hash)'
 
 if [ -f "$file_path" ]; then
   data=$(cat "$file_path")
-  all_hash_exports=$(jq "${jq_query}" <<< "$data") 
+  all_hash_exports=$(jq --arg branch "$branch_name" "${jq_query}" <<< "$data") 
   # echo "$all_hash_exports" | sed 's/"//g'
   for keyval in $(echo "$all_hash_exports" | sed 's/"//g'); do
     echo "$keyval"
